20a3193e0f2c1ac1419e642f453e936541b539e3
Deva Sai Sunder Tangella <79849464+devt-code@users.noreply.github.com>
2022-12-19 20:18:17 +0530

    Update prediciton_system.py

--- prediciton_system.py
+++ prediciton_system.py
@@ -42,33 +42,18 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratif
 
 print(X.shape, X_train.shape, X_test.shape)
 
-model = LogisticRegression()
-
-# training the LogisticRegression model with Training data
-model.fit(X_train, Y_train)
-
-# accuracy on training data
-X_train_prediction = model.predict(X_train)
-training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
-
-print('Accuracy on Training data : ', training_data_accuracy)
-
-# accuracy on test data
-X_test_prediction = model.predict(X_test)
-test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
-
-
-print('Accuracy on Test data : ', test_data_accuracy)
-
-input_data = (62,0,0,140,268,0,0,160,0,3.6,0,2,2)
-
-# change the input data to a numpy array
+from sklearn.ensemble import RandomForestClassifier
+rf = RandomForestClassifier()
+rf.fit(X_train, Y_train)
+Y_pred = rf.predict(X_test)
+from sklearn.metrics import roc_curve, auc
+false_positive_rate, true_positive_rate, thresholds = roc_curve(Y_test, Y_pred)
+roc_auc = auc(false_positive_rate, true_positive_rate)
+print(roc_auc)
+input_data = (55,0,1,132,342,0,1,166,0,1.2,2,0,2)
 input_data_as_numpy_array= np.asarray(input_data)
-
-# reshape the numpy array as we are predicting for only on instance
 input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
-
-prediction = model.predict(input_data_reshaped)
+prediction = rf.predict(input_data_reshaped)
 print(prediction)
 
 if (prediction[0]== 0):
